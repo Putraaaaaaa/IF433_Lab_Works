81511fe344d0e9d5f6258be892ee39eb88729818
Putraaaaaaa <gadingkelana1805@gmail.com>
2026-02-06 22:40:42 +0700

    week01: initial setup and procedural code

---
+++ src/oop_111323_GadingKelanaPutra/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_111323_GadingKelanaPutra.week01
+
+fun main(){
+    var name: String = "Gading Kelana"
+    var score: Int = 100
+
+    println("Nama : " + name + ". Nilai : " + score)
+}
