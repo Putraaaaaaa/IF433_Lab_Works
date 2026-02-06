a60c7559a0759008c62a2167b1f951921d43c23a
Putraaaaaaa <gadingkelana1805@gmail.com>
2026-02-06 23:43:02 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_111323_GadingKelanaPutra/week01/GameStore.kt
+++ src/oop_111323_GadingKelanaPutra/week01/GameStore.kt
@@ -2,21 +2,14 @@ package oop_001_johnthor.week01
 
 fun main() {
     val gameTitle = "Elden Ring"
-    val price = 600000
-    val discount = calculateDiscount(price)
+    val price = 750000
 
-    val userNote: String? = null
-    val finalNote = userNote ?: "Tidak ada catatan" [cite: 259]
-
-    printReceipt(title = gameTitle, finalPrice = price - discount, note = finalNote)
+    val finalPrice = calculateDiscount(price)
 }
 
 fun calculateDiscount(price: Int): Int =
-    if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
-
-fun printReceipt(title: String, finalPrice: Int, note: String) {
-    println("--- Struk Pembelian ---")
-    println("Judul: $title")
-    println("Harga Akhir: Rp $finalPrice")
-    println("Catatan: $note")
-}
+    if (price > 500_000) {
+        price - (price * 20 / 100)
+    } else {
+        price - (price * 10 / 100)
+    }
