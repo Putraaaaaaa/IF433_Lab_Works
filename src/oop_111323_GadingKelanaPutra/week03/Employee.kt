caed88dec4d081a24804a19ad6ea338554019828
Putraaaaaaa <gadingkelana1805@gmail.com>
2026-02-20 22:56:39 +0700

    week03: simulate stack overflow error in setter

--- src/oop_111323_GadingKelanaPutra/week03/Employee.kt
+++ src/oop_111323_GadingKelanaPutra/week03/Employee.kt
@@ -2,4 +2,9 @@ package oop_111323_GadingKelanaPutra.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencba set gaji ke: $value")
+            this.salary= value
+        }
 }
+
---
+++ src/oop_111323_GadingKelanaPutra/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_111323_GadingKelanaPutra.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000 // Siap-siap meledugg
+}
