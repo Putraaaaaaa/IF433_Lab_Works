bb735c6d38d1fca335c3ed1bb97271e3df052c1a
Putraaaaaaa <gadingkelana1805@gmail.com>
2026-02-20 23:16:15 +0700

    week03: cleanup code

--- src/oop_111323_GadingKelanaPutra/week03/Main.kt
+++ src/oop_111323_GadingKelanaPutra/week03/Main.kt
@@ -3,7 +3,6 @@ package oop_111323_GadingKelanaPutra.week03
 fun main() {
     val e = Employee("Budi")
 
-    e.salary = -1000 //Harusnya Print Error
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
