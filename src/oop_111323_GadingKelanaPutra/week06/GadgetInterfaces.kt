e281108dc1d88cf6125c7f9426124a5134c77548
Putraaaaaaa <gadingkelana1805@gmail.com>
2026-03-13 22:55:04 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_111323_GadingKelanaPutra/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_111323_GadingKelanaPutra.week06
+
+interface Camera {
+    fun turnon() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnon() { println("Sinyal seluler mencari jaringan.") }
+}
