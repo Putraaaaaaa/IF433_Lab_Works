674e33a1f6e9731874ccdc94018c25b4dfa73607
Putraaaaaaa <gadingkelana1805@gmail.com>
2026-04-17 23:22:35 +0700

    week08: create nested classes for Order

--- src/oop_111323_GadingKelanaPutra/week08/NotificationService.kt
+++ src/oop_111323_GadingKelanaPutra/week08/NotificationService.kt
@@ -1,4 +1,15 @@
 package oop_111323_GadingKelanaPutra.week08
 
 class NotificationService {
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile) {
+        if (user.email != null) {
+            sendEmail(emailAddress = user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
 }
