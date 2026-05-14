5359af9ad13c2aa29986c00605baa6eabf840772
Putraaaaaaa <gadingkelana1805@gmail.com>
2026-05-14 23:34:42 +0700

    week11: (task) calculate total power usage

--- src/oop_111323_GadingKelanaPutra/week11/SmartHomeDashboard.kt
+++ src/oop_111323_GadingKelanaPutra/week11/SmartHomeDashboard.kt
@@ -30,3 +30,6 @@ fun main() {
     with(homeDevices) {
         println("Jumlah perangkat: ${this.size}")
     }
+
+    val totalPower = homeDevices.run { sumOf { it.powerLoad } }
+    println("Total penggunaan daya: $totalPower Watt")
