c9767322c58651cbd16af20ea9f56b44cfaef157
Putraaaaaaa <gadingkelana1805@gmail.com>
2026-05-14 23:05:26 +0700

    week10: (task) populate coin repository

--- src/oop_111323_GadingKelanaPutra/week10/CryptoDashboard.kt
+++ src/oop_111323_GadingKelanaPutra/week10/CryptoDashboard.kt
@@ -3,7 +3,7 @@ package oop_111323_GadingKelanaPutra.week10
 fun main() {
     val coinRepo = WalletRepository<Coin>()
 
-    // Checkpoint 16: Populate
+    // Checkpoint 16: Populatee
     coinRepo.add(Coin("BTC", 0.5))
     coinRepo.add(Coin("ETH", 2.0))
     coinRepo.add(Coin("USDT", 100.0))
