f1cab18d40d1dd7692c95942c9d9c2f4fc0ac79e
Putraaaaaaa <gadingkelana1805@gmail.com>
2026-05-14 23:05:03 +0700

    week10: (task) initialize coin repository

---
+++ src/oop_111323_GadingKelanaPutra/week10/CryptoDashboard.kt
@@ -0,0 +1,10 @@
+package oop_111323_GadingKelanaPutra.week10
+
+fun main() {
+    val coinRepo = WalletRepository<Coin>()
+
+    // Checkpoint 16: Populate
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 100.0))
+}
