fdb2067578f718cc401861588a043c0b59b0a98d
Putraaaaaaa <gadingkelana1805@gmail.com>
2026-05-14 23:27:59 +0700

    week11: implement run function for object computation

--- src/oop_111323_GadingKelanaPutra/week11/ScopeConfiguration.kt
+++ src/oop_111323_GadingKelanaPutra/week11/ScopeConfiguration.kt
@@ -1,11 +1,3 @@
 package oop_111323_GadingKelanaPutra.week11
 
 data class User(var name: String = "", var age: Int = 0)
-
-fun main() {
-    println("=== TEST APPLY ===")
-    val user = User().apply {
-        name = "Alex"
-        age = 25
-    }
-    println(user)
